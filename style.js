5d4548b7352b3befed8de87ac9fa446d8b0e7cff
monibms <92415922+monibms@users.noreply.github.com>
2022-08-02 00:06:11 +0900

    Update style.js

--- style.js
+++ style.js
@@ -2,22 +2,6 @@ function toggleNav() {
   const body = document.body;
   const hamburger = document.getElementById("js-hamburger");
   const overlay = document.getElementById("js-overlay");
-  
-
-if (window.matchMedia('(max-width: 767px)').matches) {
-        const targetOffsetTopSP = window.pageYOffset + targetElement.getBoundingClientRect().top; //ここに- 50 などと数値を入れるとヘッダー固定のスクロールが実現できる
-        window.scrollTo({
-          top: targetOffsetTopSP,
-          behavior: "smooth"
-        });
-      } else if (window.matchMedia('(min-width:768px)').matches) {
-        const targetOffsetTopPC = window.pageYOffset + targetElement.getBoundingClientRect().top - 38; //ここに- 50 などと数値を入れるとヘッダー固定のスクロールが実現できる
-        window.scrollTo({
-          top: targetOffsetTopPC,
-          behavior: "smooth"
-        });
-      }
-
   hamburger.addEventListener("click", function () {
     body.classList.toggle("nav-open");
   });
